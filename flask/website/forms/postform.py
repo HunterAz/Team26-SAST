a7490034fcc1aafd22db785a8bcf15d231166387
HunterAz <55978269+HunterAz@users.noreply.github.com>
2021-11-18 09:44:09 -0800

    revert -merge?

--- flask/website/forms/postform.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, validators
-
-class PostForm(FlaskForm):
-    body = TextAreaField("Body", [validators.Length(max=1000), validators.DataRequired(
-    )], render_kw={"placeholder": "Type your comments here"})
