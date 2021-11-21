91d40b6c0d46cdc125d5bb2cff89311c253de61c
HunterAz <azfarpro@outlook.com>
2021-11-21 13:18:19 +0800

    Remove for doing by pull request

--- flask/website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint('views',__name__,template_folder='views')
-
-@views.route('/')
-def login():
-    return render_template("user/login.html")
